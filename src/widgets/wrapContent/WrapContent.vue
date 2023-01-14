961a2a72f2a33e13414187864b8bc49e24e29c36
Dalinte <kollekcionerssd1@gmail.com>
2023-01-14 18:40:36 +0300

    add wrapContent component

---
+++ src/widgets/wrapContent/WrapContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrap-content">
+    <section class="wrap-content__left">
+      <slot name="left"></slot>
+    </section>
+    <section class="wrap-content__right">
+      <slot name="right"></slot>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "WrapContent"
+})
+</script>
+
+<style scoped lang='scss'>
+  .wrap-content {
+    display: grid;
+    grid-template-columns: 1fr 525px;
+  }
+</style>
