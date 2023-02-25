ecd8bd8ea935434637c68cea77a7a35c05492a4e
Dalinte <kollekcionerssd1@gmail.com>
2023-02-25 16:18:08 +0300

    remove AboutView

--- src/app/providers/router/index.ts
+++ src/app/providers/router/index.ts
@@ -8,14 +8,6 @@ const routes: Array<RouteRecordRaw> = [
     name: 'home',
     component: SwapPage,
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '@/pages/AboutView.vue'),
-  },
 ]
 
 export const router = createRouter({
